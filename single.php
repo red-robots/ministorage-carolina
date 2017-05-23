302f7b98ac9b97152d852dc0354d9de6399cef60
reneeobrien <reneelobrien@gmail.com>
2017-05-23 15:03:34 -0400

    sidebar on single and styled sidebar

--- single.php
+++ single.php
@@ -11,22 +11,33 @@ get_header(); ?>
 
 
 <div class="wrapper">
+<div class="widget-area">
+  <h2>Recent Posts</h2>
+    <ul>
+    <?php
+      $recent_posts = wp_get_recent_posts();
+      foreach( $recent_posts as $recent ){
+        echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
+      }
+      wp_reset_query();
+    ?>
+    </ul>
+</div> <!-- widget-area -->
 	<div id="content" role="main">
-
-		<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 			          
 		            
-		<div class="entry-content">
+		<div class="entry-content single">
+
 			<h1><?php the_title(); ?></h1>
 			<?php the_content(); ?>
 		 </div>             
 		            
 
 		<?php endwhile; // end of the loop. ?>
-
+		
 	</div><!-- #content -->
-	        
-	</div>
+</div>
  <!-- wrapper -->
 
 <?php get_footer(); ?>
