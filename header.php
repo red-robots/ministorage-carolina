23e493d518bbc0b3535680b8f4e544f8b39baa0c
Austin Crane <austincrane@gmail.com>
2017-07-13 10:55:33 -0400

    add schema

--- header.php
+++ header.php
@@ -29,6 +29,199 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <!-- lw2 -->
+<script type="application/ld+json">
+{
+  "@context" : "http://schema.org",
+  "@type" : "Organization",
+  "name" : "The Mini Storage Center",
+  "image" : "http://ministoragecarolina.com/bw/wp-content/themes/ministorage-carolina/images/logo.png",
+  "department": 
+    [
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Charlotte",
+                "addressRegion": "NC",
+                "postalCode": "28208",
+                "streetAddress": "2318 Freedom Dr"
+            },
+            "telephone" : "(704) 393-1425",
+            "email" : "fd@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Charlotte",
+                "addressRegion": "NC",
+                "postalCode": "28205",
+                "streetAddress": "1515 E. Sugar Creek Rd"
+            },
+            "telephone" : "(704) 375-7983",
+            "email" : "sc@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Charlotte",
+                "addressRegion": "NC",
+                "postalCode": "28205",
+                "streetAddress": "4500 Monroe Rd"
+            },
+            "telephone" : "(704) 333-7064",
+            "email" : "mr@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Charlotte",
+                "addressRegion": "NC",
+                "postalCode": "28209",
+                "streetAddress": "4475 South Blvd"
+            },
+            "telephone" : "(704) 523-1156",
+            "email" : "sb@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Charlotte",
+                "addressRegion": "NC",
+                "postalCode": "28273",
+                "streetAddress": "700-A Westinghouse Blvd"
+            },
+            "telephone" : "(704) 588-3915",
+            "email" : "wb@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Columbia",
+                "addressRegion": "SC",
+                "postalCode": "29169",
+                "streetAddress": "730 Chris Dr"
+            },
+            "telephone" : "(803) 794-4228"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Conway",
+                "addressRegion": "SC",
+                "postalCode": "29526",
+                "streetAddress": "490 University Forest Dr"
+            },
+            "telephone" : "(843) 349-9800",
+            "email" : "501@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Conway",
+                "addressRegion": "SC",
+                "postalCode": "29526",
+                "streetAddress": "1488 Hwy 501"
+            },
+            "telephone" : "(843) 349-9800",
+            "email" : "501@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Conway",
+                "addressRegion": "SC",
+                "postalCode": "29526",
+                "streetAddress": "1480 Hwy 501 Bus"
+            },
+            "telephone" : "(843) 349-9800",
+            "email" : "501@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Garden City",
+                "addressRegion": "SC",
+                "postalCode": "29576",
+                "streetAddress": "12053 Hwy 17 Bypass"
+            },
+            "telephone" : "(843) 651-0440",
+            "email" : "gc@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Garden City",
+                "addressRegion": "SC",
+                "postalCode": "29576",
+                "streetAddress": "605 Boundary Ave"
+            },
+            "telephone" : "(843) 651-0440",
+            "email" : "gc@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Garden City",
+                "addressRegion": "SC",
+                "postalCode": "29576",
+                "streetAddress": "565 Pine Ave"
+            },
+            "telephone" : "(843) 651-0440",
+            "email" : "gc@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Myrtle Beach",
+                "addressRegion": "SC",
+                "postalCode": "29588",
+                "streetAddress": "Coastal Storage & Retail"
+            },
+            "telephone" : "(843) 650-6802",
+            "email" : "707@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Rock Hill",
+                "addressRegion": "SC",
+                "postalCode": "29732",
+                "streetAddress": "1370 Constitution Blvd"
+            },
+            "telephone" : "(803) 985-3660",
+            "email" : "rh@ministoragecarolina.com"
+        },
+        {
+            "@type": "Storage Center",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Spartanburg",
+                "addressRegion": "SC",
+                "postalCode": "29301",
+                "streetAddress": "350 E. Blackstock Rd"
+            },
+            "telephone" : "(864) 587-9515",
+            "email" : "sp@ministoragecarolina.com"
+        },
+    ],
+  "url" : "https://ministoragecarolina.com/",
+  
+}
+</script>
+
 <?php wp_head(); ?>
 
 
